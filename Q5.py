2d3bbe60be4fd76d7d5896268dd38090058ca4af
Gaurav Dua <gaurav3dua@gmail.com>
2019-01-14 10:36:39 +0530

    Submission for Q5
    Create a basic Flask API named ‘Hello World’, which takes blob path for Excel file and sends blob
    path of the modified file in response
    a. API should have 2 end-points with GET and POST method
    b. GET method should return API name in response
    c. POST method accepts a JSON request body with blob path and returns blob path in
    response
    
    What this API does?
    • Downloads Excel file
    • Data contains item_id, supplier_id and supplier_val columns
    • API should create a new column named supplier_rank
    • API ranks supplier for every line item according to the supplier_val
    • Ranks shouldn’t have breaks
    • If more than one supplier has same supplier_val their rank should be same
    • Uploads output excel
    • Keep output filename as inputfilename_yourname

---
+++ Q5.py
@@ -0,0 +1,71 @@
+import os
+import sys
+from azure.storage.blob import BlockBlobService
+import re
+from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
+from scipy.stats import rankdata as rd
+
+app = Flask(__name__)
+my_result = {"result": "Enter URL", "uploaded_url": "none"}
+
+
+@app.route('/', methods=['GET'])
+def get_result():
+    return jsonify(my_result)
+
+
+@app.route('/processjson', methods=['POST'])
+def processjson():
+    data = request.get_json()
+
+    # extract container name and file name from url using regex
+    my_url = data['my_url']
+    pattern = re.compile(r'(?<=net\/).*?(?=\/)')
+    matches = pattern.findall(my_url)
+    container_name = matches[0]
+    my_reg = r"(?<=" + container_name + "\/).*"
+    pattern = re.compile(my_reg)
+    matches = pattern.findall(my_url)
+    local_file_name = matches[0]
+
+
+    # download file
+    block_blob_service = BlockBlobService(account_name='dsconvreport',
+                                          account_key='2wIt3xVY2HR5mXfl2489ctyE1CIewgwA0am+jE85HkOfOBKc7Af0KHHb2YS9Z466T+v9KClZXYeht21M3oXFYw==')
+    full_path_to_file = local_file_name[:-5]+"_downloaded.xlsx"
+    block_blob_service.get_blob_to_path(container_name, local_file_name, full_path_to_file)
+
+    # process data
+    df = pd.read_excel(full_path_to_file)
+    b = df.values.tolist()
+    my_sup_val = []
+    my_items = []
+    my_ranks = []
+    for i in b:
+        if i[0] not in my_items:
+            my_items.append(i[0])
+
+    for j in my_items:
+        for i in b:
+            if i[0] == j:
+                my_sup_val.append(i[2])
+        my_ranks.extend(rd(my_sup_val, method="dense"))
+        my_sup_val = []
+
+    # make new file
+    df["supplier_rank"] = my_ranks
+    new_file = "Test SupplierData_gaurav.xlsx"
+    df.to_excel(new_file)
+
+    # Upload file
+    block_blob_service.create_blob_from_path(container_name, local_file_name, new_file)
+
+    output_url = "https://dsconvreport.blob.core.windows.net/"+container_name+"/"+ full_path_to_file
+    my_result['result'] = "Uploaded Successfully"
+    my_result['uploaded_url'] = output_url
+    return jsonify({'output_url': output_url, 'Message': 'Successfully Downloaded and Uploaded'})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
